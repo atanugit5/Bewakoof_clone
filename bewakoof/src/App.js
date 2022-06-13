403e8335797e07f09f9d36cf680fe03a7cd2e21a
Atanu Mondal <atanu.mondal.me48@gmail.com>
2022-06-13 19:22:46 +0530

    bewakoof

---
+++ bewakoof/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Heading,Image } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className="App">
+     <Heading fontSize={"xl"}>Welcome to</Heading><Image src="https://images.bewakoof.com/web/ic-desktop-pride-bwkf-logo.svg" m={"auto"}/>
+    </div>
+  );
+}
+
+export default App;
